a1c5f8c94e323fb197f41a3a7baf782743fb17ea
lavabear <mpmcg37@aol.com>
2022-04-28 23:27:55 -0600

    update type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,12 @@
-export const defaultAdjectives : string[]
-
-export const defaultNouns: string[]
-
 export const randomNumber: (max: number) => number
+
+export const toChars: (value: number, characters?: string) => string
 
 type HaikunateProps = {
     maxValue: number,
     adjectives: string[],
-    nouns: string[]
+    nouns: string[],
+    useChars: boolean
 }
 
 declare const haikunate : (props?: HaikunateProps) => string
